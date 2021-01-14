068d993bd6d77180867e12e8fedf4e421d7e9c4b
Milena Teixeira <milenateixeira648@gmail.com>
2021-01-13 22:11:20 -0300

    JPA added.

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,20 +1,34 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente(1L, "milena teixeira", "milena@gmail.com", "12345");
-		var cliente2 = new Cliente(2L, "stephane", "stephane@gmail.com", "12345");
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		if(cliente.isPresent()) return ResponseEntity.ok(cliente.get());
+		else return ResponseEntity.notFound().build();
 	}
 }
